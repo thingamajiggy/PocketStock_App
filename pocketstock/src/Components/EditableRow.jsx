0728ecf2fcb41e92afaa3cb6e597b44ad26fa49c
Ryan Lee <ryanlee@ryans-imac.home>
2022-10-13 11:09:37 +0100

    MVP for inventory page

---
+++ pocketstock/src/Components/EditableRow.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+
+const EditableRow = ({editFormData, handleEditFormChange, handleCancelClick, handleDeleteClick}) => {
+  return (
+    <tr>
+      <td>
+        <input
+          type="text"
+          name="component"
+          required="required"
+          placeholder="Enter component name"
+          onChange={handleEditFormChange}
+          value={editFormData.component}
+        />
+      </td>
+      <td>
+        <input
+          type="number"
+          name="stockLevel"
+          required="required"
+          placeholder="Enter stock level"
+          onChange={handleEditFormChange}
+          value={editFormData.stockLevel}
+        />
+      </td>
+      <td>
+        <input
+          type="number"
+          name="triggerPoint"
+          required="required"
+          placeholder="Enter trigger point"
+          onChange={handleEditFormChange}
+          value={editFormData.triggerPoint}
+        />
+      </td>
+      <td>
+        <button type="submit">Save</button>
+        <button type="button" onClick={handleCancelClick}>Cancel</button>
+        <button type="button" onClick={handleDeleteClick}>Delete</button>
+        
+      </td>
+    </tr>
+  );
+};
+
+export default EditableRow;
--- pocketstock/src/Routes/Inventory.jsx
+++ pocketstock/src/Routes/Inventory.jsx
@@ -1,30 +1,12 @@
 import React from "react";
-import { useState } from "react";
+
+import ComponentsList from "../Components/ComponentsList";
+
 
 const Inventory = () => {
   return (
     <div>
-      <table>
-      
-  <thead>
-    <tr>
-      <th>Id</th>
-      <th>Component</th>
-      <th>Current Stock</th>
-      <th>Trigger Point</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td>Cell</td>
-      <td>Cell</td>
-    </tr>
-  </tbody>
-</table>
-
-   
+      <ComponentsList />
     </div>
   );
 };
